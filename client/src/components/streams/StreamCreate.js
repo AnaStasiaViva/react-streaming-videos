ae12514ff13ca5f937bbd45a210e817796c74ce9
AnaStasiaViva <52911733+AnaStasiaViva@users.noreply.github.com>
2021-03-12 00:54:10 +0500

    Add files via upload

---
+++ client/src/components/streams/StreamCreate.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
+import StreamForm from './StreamForm'
+
+//due to helper methods class cmp
+
+class StreamCreate extends React.Component {
+   onSubmit = (formValues) => {
+    this.props.createStream(formValues)
+   }
+
+    render(){
+         return (
+       <div>
+           <h3>Create stream</h3>
+           <StreamForm onSubmit={this.onSubmit} />
+       </div>
+     )
+    }
+   
+}
+
+
+
+
+
+
+export default connect(null, {
+    createStream
+})(StreamCreate)
